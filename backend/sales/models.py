cb725e93b9fd5062420718a94cd2c0a5429ac263
bwillis32 <bwillis@bentoncorp.com>
2016-09-06 12:30:19 -0400

    added viewsets

--- backend/sales/models.py
+++ backend/sales/models.py
@@ -7,5 +7,11 @@ from django.db import models
 class Sales(models.Model):
     id = models.IntegerField(default=0, primary_key=True)
     neighborhood = models.CharField(max_length=255)
-    
-    
+    year = models.CharField(max_length=30, blank=True)
+    #zillow = ForeignKey('self',on_delete=modes. PROTECT)
+    type_of_home = models.PositiveSmallIntegerField(null = True)
+    num_of_sales = models.PositiveSmallIntegerField(null = True)
+    lowest_sale_price =models.FloatField(null = True)
+    avg_sale_price = models.FloatField(null = True)
+    med_sale_price = models.FloatField(null = True)
+    high_sale_price = models.FloatField(null = True)
--- backend/sales/views.py
+++ backend/sales/views.py
@@ -1,5 +1,31 @@
-from django.http import HttpResponse
+from rest_framework import viewsets
+from rest_framework.decorators import detail_route
 
-# Create your views here.
-def index(request):
-    return HttpResponse("Hello World.")
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+    @detail_route(renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
